85aaf4f704e0e14c2272d83942d776d9858694f0
Dhany Dwi Putra <dhany.dwi@nabatisnack.co.id>
2025-03-17 14:30:50 +0700

    adding sponsorship space

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,6 +9,7 @@ import {
   TwitterIcon,
 } from "lucide-react";
 import Link from "next/link";
+import Logos07Page from "./logos-07/logos-07";
 
 const footerLinks = [
   {
@@ -31,62 +32,65 @@ const footerLinks = [
 
 const Footer = () => {
   return (
-    <footer className="dark:border-t mt-40 dark bg-[#0b685c] text-foreground">
-      <div className="mx-auto max-w-5xl">
-        <div className="py-12 flex flex-col sm:flex-row items-start justify-between gap-x-8 gap-y-10 px-6 xl:px-0">
-          <div>
-            {/* Logo */}
-            <img src="/logo-2.svg" alt="Shadcn UI Blocks" className="h-8 w-auto" />
+    <>
+      <Logos07Page />
+      <footer className="dark:border-t mt-20 dark bg-[#0b685c] text-foreground">
+        <div className="mx-auto max-w-5xl">
+          <div className="py-12 flex flex-col sm:flex-row items-start justify-between gap-x-8 gap-y-10 px-6 xl:px-0">
+            <div>
+              {/* Logo */}
+              <img src="/logo-2.svg" alt="Shadcn UI Blocks" className="h-8 w-auto" />
 
-            <ul className="mt-6 flex items-center gap-4 flex-wrap">
-              {footerLinks.map(({ title, href }) => (
-                <li key={title}>
-                  <Link
-                    href={href}
-                    className="text-white hover:text-foreground"
-                  >
-                    {title}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+              <ul className="mt-6 flex items-center gap-4 flex-wrap">
+                {footerLinks.map(({ title, href }) => (
+                  <li key={title}>
+                    <Link
+                      href={href}
+                      className="text-white hover:text-foreground"
+                    >
+                      {title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            {/* Download App Section */}
+            <div className="flex flex-col items-start">
+              <h3 className="text-white text-lg font-semibold mb-2">Download Aplikasi</h3>
+              <Link 
+                href="https://play.google.com/store/apps/details?id=com.furgetech.shollu" 
+                target="_blank"
+              >
+                <img 
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/2560px-Google_Play_Store_badge_EN.svg.png" 
+                  alt="Download di Play Store" 
+                  className="h-12 w-auto"
+                />
+              </Link>
+            </div>
+
           </div>
+          <Separator />
+          <div className="py-8 flex flex-col-reverse sm:flex-row items-center justify-between gap-x-2 gap-y-5 px-6 xl:px-0">
+            {/* Copyright */}
+            <span className="text-white text-center sm:text-start">
+              &copy; {new Date().getFullYear()}{" "}
+              <Link href="/" target="_blank">
+                Shollu
+              </Link>
+              . All rights reserved.
+            </span>
 
-          {/* Download App Section */}
-          <div className="flex flex-col items-start">
-            <h3 className="text-white text-lg font-semibold mb-2">Download Aplikasi</h3>
-            <Link 
-              href="https://play.google.com/store/apps/details?id=com.furgetech.shollu" 
-              target="_blank"
-            >
-              <img 
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/2560px-Google_Play_Store_badge_EN.svg.png" 
-                alt="Download di Play Store" 
-                className="h-12 w-auto"
-              />
-            </Link>
+            <div className="flex items-center gap-5 text-white">
+              <Link href="https://www.instagram.com/sholatchampions" target="_blank">
+                <Instagram className="h-5 w-5" />
+              </Link>
+            </div>
           </div>
-
         </div>
-        <Separator />
-        <div className="py-8 flex flex-col-reverse sm:flex-row items-center justify-between gap-x-2 gap-y-5 px-6 xl:px-0">
-          {/* Copyright */}
-          <span className="text-white text-center sm:text-start">
-            &copy; {new Date().getFullYear()}{" "}
-            <Link href="/" target="_blank">
-              Shollu
-            </Link>
-            . All rights reserved.
-          </span>
-
-          <div className="flex items-center gap-5 text-white">
-            <Link href="https://www.instagram.com/sholatchampions" target="_blank">
-              <Instagram className="h-5 w-5" />
-            </Link>
-          </div>
-        </div>
-      </div>
-    </footer>
+      </footer>
+    </>
   );
 };
 
---
+++ components/logos-07/logos-07.tsx
@@ -0,0 +1,39 @@
+import Marquee from "@/components/ui/marquee";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { ArrowUpRight, PhoneCall } from "lucide-react";
+
+const Logos07Page = () => {
+  return (
+    <div className="flex items-center justify-center px-6">
+      <div className="overflow-hidden">
+        <p className="text-center text-xl font-medium">
+          Daftar Donatur Shollu se-Sumatera Barat
+        </p>
+
+        <div className="mt-5 max-w-screen-6xl space-y-3">
+          <Marquee pauseOnHover className="[--duration:30s] [&_svg]:mr-10">
+            <Image alt="sponsor" src={'/sponsor_space.svg'} width={200} height={120} />
+          </Marquee>
+          <Marquee
+            pauseOnHover
+            reverse
+            className="[--duration:30s] [&_svg]:mr-10"
+          >
+            <Image alt="sponsor" src={'/sponsor_space.svg'} width={200} height={120} />
+          </Marquee>
+        </div>
+        <div className="relative w-full justify-center items-center z-0 mt-8 flex flex-col sm:flex-row gap-4">
+          <Link href="https://wa.me/6285215003995?text=Assalamualaikum%20Admin%20Masjid%20Kami%20Ingin%20Bergabung%20dengan%20Kalian">
+            <Button size="lg">
+              <PhoneCall className="!h-5 !w-5" /> Jadi Sponsor
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Logos07Page;
