63db2710702110e5184d2aa1e68c56d524e7a35d
Dhany Dwi Putra <dhany.dwi@nabatisnack.co.id>
2025-03-18 14:24:47 +0700

    adding hero slide

--- components/hero.tsx
+++ components/hero.tsx
@@ -10,15 +10,6 @@ import Link from "next/link";
 import { link } from "fs";
 
 const heroSlides = [
-  // {
-  //   title: "Pejuang Qur'an",
-  //   description: `
-  //     Pejuang Qur'an adalah program tilawah Al-Quran yang di rancang khusus untuk anak-anak dan remaja dengan target minimal satu kali khatam. Dengan pencatatan berbasis QR code
-  //   `,
-  //   image: "https://images.unsplash.com/photo-1683355879158-8f4d2225067c?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  //   cta: "Lihat Detail Event",
-  //   link: '/pejuang-quran'
-  // },
   {
     title: "Smart I'tikaf",
     description: `
@@ -29,15 +20,24 @@ const heroSlides = [
     link: '/smart-itikaf',
     link2: '/smart-itikaf/register'
   },
-  // {
-  //   title: "Sholat Champions",
-  //   description: `
-  //     Sholat Champions adalah Program Kompetisi dan Monitoring Sholat bagi anak-anak dan remaja untuk membiasakan Sholat berjamaah di Masjid. Dengan menggunakan sistem QR Code atau Finger print. Peserta terbaik akan mendapatkan hadiah
-  //   `,
-  //   image: "https://images.unsplash.com/photo-1616261371178-9e9490303aef?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  //   cta: "Lihat Detail Event",
-  //   link: '/sholat-champions'
-  // }
+  {
+    title: "Pejuang Qur'an",
+    description: `
+      Pejuang Qur'an adalah program tilawah Al-Quran yang di rancang khusus untuk anak-anak dan remaja dengan target minimal satu kali khatam. Dengan pencatatan berbasis QR code
+    `,
+    image: "https://images.unsplash.com/photo-1683355879158-8f4d2225067c?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    cta: "Lihat Detail Event",
+    link: '/pejuang-quran'
+  },
+  {
+    title: "Sholat Champions",
+    description: `
+      Sholat Champions adalah Program Kompetisi dan Monitoring Sholat bagi anak-anak dan remaja untuk membiasakan Sholat berjamaah di Masjid. Dengan menggunakan sistem QR Code atau Finger print. Peserta terbaik akan mendapatkan hadiah
+    `,
+    image: "https://images.unsplash.com/photo-1616261371178-9e9490303aef?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    cta: "Lihat Detail Event",
+    link: '/sholat-champions'
+  }
 ];
 
 const Hero = () => {
@@ -50,7 +50,7 @@ const Hero = () => {
           spaceBetween={0}
           slidesPerView={1}
           pagination={{ clickable: true }}
-          autoplay={{ delay: 5000 }}
+          autoplay={{ delay: 10000 }}
           loop={true}
           className="h-full"
         >
